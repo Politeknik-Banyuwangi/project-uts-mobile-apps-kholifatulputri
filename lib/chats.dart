c57fa58708140991f0daccca0ffd779884b96289
FlutLab.io <>
2022-05-31 05:15:14 +0000

    Autosave

--- lib/chats.dart
+++ lib/chats.dart
@@ -17,6 +17,7 @@ class Chats extends StatelessWidget {
             SizedBox(
               height: 10,
             ),
+            chatData("Fira", "Put tugasmu gimana", "23.44", Icons.done_all, Colors.blue, NetworkImage("https://asset.kompas.com/crops/W4P3no-vmu-9GnsCWwybHDufncM=/120x34:4222x2768/750x500/data/photo/2021/04/18/607ba95f8d6f7.jpg"))
           ],
         ),
       ),
@@ -27,6 +28,37 @@ class Chats extends StatelessWidget {
           Icons.chat_bubble,
           color: Colors.white,
         ),
+      ),
+    );
+  }
+
+  Widget chatData(String name, String chat, String time, IconData centang, Color warna, NetworkImage networkImage) {
+    return Card(
+      margin: EdgeInsets.only(bottom: 0.9),
+      child: ListTile(
+        leading: CircleAvatar(
+          backgroundImage: networkImage,
+          radius: 30,
+        ),
+        title: Text(
+          name,
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+        ),
+        subtitle: Row(children: [
+          Icon(
+            centang,
+            size: 16,
+            color: warna,
+          ),
+          SizedBox(width: 5),
+          Text(
+            chat,
+            style: TextStyle(
+              fontSize: 13,
+            ),
+          ),
+        ]),
+        trailing: Text(time),
       ),
     );
   }
